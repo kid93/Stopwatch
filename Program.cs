cefaa5b285c86e70196759e5af5770fac5a1e867
kid93 <uikid93@gmail.com>
2017-06-04 19:33:57 +0100

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Stopwatch
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            stopwatch.Stop();
+            Console.WriteLine(stopwatch.Duration);
+
+            stopwatch.Start();
+            stopwatch.Stop();
+            Console.WriteLine(stopwatch.Duration);
+
+        }
+    }
+}
---
+++ Stopwatch.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Stopwatch
+{
+    public class Stopwatch
+    {
+
+        private DateTime _start;
+        private DateTime _stop;
+        private bool _hasStarted;
+        private readonly List<TimeSpan> _totalDuration;
+
+        public TimeSpan Duration
+        {
+            get
+            {
+                var sum = TimeSpan.Zero;
+                foreach (TimeSpan timeSpan in _totalDuration)
+                {
+                    sum += timeSpan;
+                }
+                return sum;
+            }
+        }
+
+        public Stopwatch()
+        {
+            _hasStarted = false;
+            _totalDuration = new List<TimeSpan>();
+        }
+
+
+        public void Start()
+        {
+            if (!_hasStarted)
+            {
+                _start = DateTime.Now;
+                _hasStarted = true;
+            }
+            else
+            {
+                throw new InvalidOperationException(nameof(_start));
+            }
+        }
+
+        public void Stop()
+        {
+            if (_hasStarted)
+            {
+                _stop = DateTime.Now;
+                _hasStarted = false;
+
+                AddDurationToList();
+            }
+        }
+
+        private void AddDurationToList()
+        {
+            _totalDuration.Add(_stop - _start);
+        }
+
+    }
+}
